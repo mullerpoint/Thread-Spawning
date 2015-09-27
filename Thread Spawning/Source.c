be191e8c01d6c8478ad996b73fc54f8d52bad01d
mullerpoint <gary@mullerpoint.com>
2015-09-27 11:55:11 -0700

    Initial Commit  for code

---
+++ Thread Spawning/Source.c
@@ -0,0 +1,73 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Gary Muller
+// Fall 2015
+// CS 420 Assignment 1
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//A program that will spawn three child processes, print out the Parent and Child PIDs
+//it will utilize fork() and execv() to spawn the processes 
+//***specific implementations of these functions mean that this code will likely only run on RHEL***
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//Header files - Standard libraries and classes
+#include <stdio.h> //standard IO include
+#include <sys/types.h> //typedef for unistd
+#include <unistd.h> //process control functions
+#include <pthread.h> //thread control functions
+#include <errno.h> //error handling
+#include <strings.h> //strerror()
+//
+
+
+//Global Variables and Defines
+
+//
+
+//Function Prototypes
+void* threadProgram();
+//
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//Main Declaration
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void* main(int argv, char* argc[])
+{
+	//create variables
+	pthread_t tid; //thread identifier
+	int result;
+
+	//print initial data
+	printf("main: main thread here; pid is %lu, tid is %lu", getpid(), gettid());
+
+	//spawn new thread
+	result = pthread_create(&tid, NULL, thread(), NULL);
+
+
+	//check results and complete program accordingly
+	if (result == 0)//successful creation of a seccond thread
+	{
+		printf("main: successfully created a new thread with TID of %lu", tid);
+		pthread_join(tid, NULL);
+	}
+	else //unsucessful creation of a seccond thread
+	{
+		printf(" ERR: error at line %d, error: %s", __LINE__, strerror(result));
+	}
+
+	
+
+	printf("main: second thread has terminated, main will now exit.");
+}
+
+
+
+
+
+void* threadProgram()
+{
+	printf("\tsecond thread here, my tid is %lu but I'm still in process %lu", gettid(), getpid());
+}
